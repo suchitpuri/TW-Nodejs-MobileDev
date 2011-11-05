fe57e11aec78f9938bfc9c3311647f1f84b4cb3c
suchitp <suchitp@thoughtworks.com>
2011-11-06 00:16:40 +0530

    adding method to compute next lat long from a given point

--- services/paintball_services/models/player.js
+++ services/paintball_services/models/player.js
@@ -8,6 +8,16 @@ var peopleKilled = [
   this.shoot = function(lat,lng,direction){
     var peopleKilledJSON = JSON.stringify(peopleKilled);
     return peopleKilledJSON;
+ 
+  }
+
+  function getNextLatLong(lat1,lon1,brng){
+    var d = .002 ; //range of the gun
+    var R = 6371 ; //radius of earth
+    var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) + 
+                                               Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng) );
+    var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1), 
+                                                              Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2));
   }
 
 }
