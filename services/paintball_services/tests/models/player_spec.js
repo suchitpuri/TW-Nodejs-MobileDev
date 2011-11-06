dd7039f48d71f29b86aafd7ed3d13eff5b7da3e1
suchitp <suchitp@thoughtworks.com>
2011-11-07 00:45:48 +0530

    adding nodeunit test case support and a test case for player

---
+++ services/paintball_services/tests/models/player_spec.js
@@ -0,0 +1,15 @@
+
+var playerModule = require('../../models/player');
+
+var peopleKilled = [
+    { name: 'Suchit', location: 'gurgaon' },
+    { name: 'Atul', location: 'sao polo' },
+    { name: 'Vineet', location: 'london' }
+  ];
+
+
+exports['shoot should return killed players'] = function (test) {
+      var player = new playerModule.Player();
+      test.equal(player.shoot(123,123,abc), JSON.stringify(peopleKilled));
+      test.done();
+};
